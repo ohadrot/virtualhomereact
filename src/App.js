6614ba460bb7a7bf25325f330967b4fcc156d61d
ohad rot <ohad@svcollege.co.il>
2022-06-01 19:51:15 +0300

    new react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>react git example</h1>
+    </div>
+  );
+}
+
+export default App;
