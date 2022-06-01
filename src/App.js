0ee12b8ce469522e7ad72b3e1c276c0c334007e3
ohad rot <ohad@svcollege.co.il>
2022-06-01 20:04:42 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import Page1 from './Componente/Page1';
+import Page2 from './Componente/Page2';
 function App() {
   return (
     <div className="App">
-      <h1>react git example</h1>
+      <BrowserRouter>
+      <Link to='/'><button>page1</button></Link>
+      <Link to='/page2'><button>page2</button></Link>
+      <Routes>
+        <Route path='/' element ={<Page1/>}/>
+        <Route path='/page2' element={<Page2/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
